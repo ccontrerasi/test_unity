a3bd2cca54fa2020a87fdb8e176b67e6da1ae8be
Christian Contreras <christian.contreras@talentomobile.com>
2023-06-07 22:28:50 +0200

    Adding unity integration

---
+++ test_unity/test_unity/test_unityApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_unityApp.swift
+//  test_unity
+//
+//  Created by Cristian Contreras on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct test_unityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
